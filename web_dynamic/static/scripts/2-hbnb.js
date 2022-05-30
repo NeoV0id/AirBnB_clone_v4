8313d21dd8c359deec841dca50a8454eeb9a707a
NeoV0id <jokerghoul.kpop@gmail.com>
2022-05-29 22:55:57 -0700

    Readding 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,17 +1,20 @@
 $(document).ready(function () {
-    let myId = {};
-    $('input[type=checkbox]').change( function () {
-        if ($(this).prop('checked')) {
-            myId[$(this).attr('data-id')] = $(this).attr('data-name');
-        } else {
-            delete myId[$(this).attr('data-id')];
-        }
-    $('div.amenities h4').html(Object.values(myId).join(", "))
-    })
-    let request = new XMLHttpRequest();
-	request.open("GET", "http://0.0.0.0:5001/api/v1/status/", false);
-	request.send(null);
-    let available = web_dynamic/templates/2-hbnb.html.getElementById("available")
-	if request.responseText === "OK" {
-		available.div#api_status.add
+  const myId = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
+      myId[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete myId[$(this).attr('data-id')];
+    }
+    $('div.amenities h4').html(Object.values(myId).join(', '));
+  });
+  const request = new XMLHttpRequest();
+  request.open('GET', 'http://0.0.0.0:5001/api/v1/status/', false);
+  request.send(null);
+  const available = web_dynamic / templates / 2 - hbnb.html.getElementById('available');
+	    if (request.responseText === 'OK') {
+		    $('#api_status').addClass(available);
+  } else {
+    $('#api_status').removeClass(available);
+  }
 });
